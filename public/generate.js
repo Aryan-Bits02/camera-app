47dbcaf4602b11885cadedf3ec3d84189da5a64c
Aryan-Bits02 <prasadaryan87923@gmail.com>
2026-04-19 15:26:06 +0530

    reduce frame rate for streaming

--- public/generate.js
+++ public/generate.js
@@ -1,6 +1,8 @@
 const QRCode = require('qrcode');
-const url = 'https://yourdomain.com';  // replace with your public URL
+
+const url = 'https://camera-app-production-6cc4.up.railway.app/index.html';
+
 QRCode.toFile('qr.png', url, { errorCorrectionLevel: 'H' }, err => {
-  if (err) console.error(err);
-  else console.log('QR code saved as qr.png');
+    if (err) console.error(err);
+    else console.log('QR code saved as qr.png');
 });
